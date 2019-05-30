909098965010ba95bedd11f58b00f865d86d3111
OlisGithub <aritra.chakraborty1996@gmail.com>
2019-05-30 16:44:03 +0530

    Employee model

---
+++ DemoGit/src/Employee.java
@@ -0,0 +1,24 @@
+
+public class Employee {
+		private int empId;
+		private String empNmae;
+		private double empSal;
+		public int getEmpId() {
+			return empId;
+		}
+		public void setEmpId(int empId) {
+			this.empId = empId;
+		}
+		public String getEmpNmae() {
+			return empNmae;
+		}
+		public void setEmpNmae(String empNmae) {
+			this.empNmae = empNmae;
+		}
+		public double getEmpSal() {
+			return empSal;
+		}
+		public void setEmpSal(double empSal) {
+			this.empSal = empSal;
+		}
+}
