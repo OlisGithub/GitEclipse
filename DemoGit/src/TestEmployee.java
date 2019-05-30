94426935c84528c3cd558b5de6f7b07733e9044f
OlisGithub <aritra.chakraborty1996@gmail.com>
2019-05-30 16:52:16 +0530

    Main method

---
+++ DemoGit/src/TestEmployee.java
@@ -0,0 +1,13 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		Employee e1=new Employee();
+		e1.setEmpId(101);
+		e1.setEmpNmae("Aritra");
+		e1.setEmpSal(10000);
+		
+
+	}
+
+}
